8e01ffdc29a6f87e7d3462f9ca7299c871a1c5c0
Sergey Prokopchuk <svp@anahoret.com>
2016-01-08 09:26:37 +0200

    Add some tests for checks. Exercise 1.7

--- exercise_1_8.rkt
+++ exercise_1_8.rkt
@@ -2,16 +2,15 @@
 
 (require rackunit)
 
+(define (cube n) (* n n n))
 (define (square n) (* n n))
 
-(define (average x y)
-   (/ (+ (/ x (square y)) (* 2 y)) 3))
 
 (define (improve guess x)
-  (average guess (/ x guess)))
+  (/ (+ (/ x (square guess)) (* 2 guess)) 3))
 
 (define (good-enough? guess x)
-  (< (abs (- guess (improve guess x))) 0.001))
+  (< (abs (- guess (improve guess x))) 1e-10))
 
 
 (define (cube-iter guess x)
@@ -25,6 +24,26 @@
       0
       (cube-iter 1.0 x)))
 
+<<<<<<< HEAD
+<<<<<<< HEAD
 (cube-root 27) 
+=======
+(cube-root 27)
+>>>>>>> 535e74a... Add some tests. Exercise 1.8
 
+(cube (cube-root 27)) ;;; 27.000014608735402
 
+<<<<<<< HEAD
+=======
+(check-= (cube-root 27) 3 1e-10) ;;; 
+
+(check-= (cube-root 1e-28) 0 1e-5);;; 
+
+(check-= (cube-root 1e+25) 215443469.0031881 1e-5);;;
+
+>>>>>>> f6a95a5... Add tests for checks. Exercise 1.8
+=======
+(cube (cube-root 1e-28));;; 1.6983889754480276e-28
+
+(cube (cube-root 1e+45));;;1.0000000000000001e+45
+>>>>>>> 535e74a... Add some tests. Exercise 1.8
