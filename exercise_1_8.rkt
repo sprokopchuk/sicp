44f205520eec77e5d1eaf6c5eaf67b2b12f7c40b
Sergey Prokopchuk <svp@anahoret.com>
2016-01-08 17:41:57 +0200

    Clean up merging. Exercise 1.8

--- exercise_1_8.rkt
+++ exercise_1_8.rkt
@@ -24,43 +24,10 @@
       0
       (cube-iter 1.0 x)))
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-(cube-root 27) 
-=======
-(cube-root 27)
->>>>>>> 535e74a... Add some tests. Exercise 1.8
 
-(cube (cube-root 27)) ;;; 27.000014608735402
-
-<<<<<<< HEAD
-=======
 (check-= (cube-root 27) 3 1e-10) ;;; 
 
 (check-= (cube-root 1e-28) 0 1e-5);;; 
 
 (check-= (cube-root 1e+25) 215443469.0031881 1e-5);;;
 
->>>>>>> f6a95a5... Add tests for checks. Exercise 1.8
-=======
-(cube (cube-root 1e-28));;; 1.6983889754480276e-28
-
-(cube (cube-root 1e+45));;;1.0000000000000001e+45
->>>>>>> 535e74a... Add some tests. Exercise 1.8
-=======
-(cube-root 27)
-=======
-(check-= (cube-root 27) 3 1e-10) ;;; 
->>>>>>> 288cab6... Add tests for checks. Exercise 1.8
-
-(check-= (cube-root 1e-28) 0 1e-5);;; 
-
-(check-= (cube-root 1e+25) 215443469.0031881 1e-5);;;
-
-<<<<<<< HEAD
-(cube (cube-root 1e+45));;;1.0000000000000001e+45
->>>>>>> 942707b... Add some tests. Exercise 1.8
-=======
->>>>>>> 288cab6... Add tests for checks. Exercise 1.8
