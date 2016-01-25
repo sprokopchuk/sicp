b37f682af98f83b3534401df7cec4c173bcf1647
Sergey Prokopchuk <svp@anahoret.com>
2016-01-25 10:25:59 +0200

    Finish exercise 1.29

--- Exercise_1_29.rkt
+++ Exercise_1_29.rkt
@@ -6,26 +6,25 @@
 
 (define (cube x) (* x x x))
 
-(define (sum term a next b k)
+(define (sum term a next b)
   (if (> a b)
       0
       (+ (term a)
-         (sum term (next a k) next b (+ k 1)))))
+         (sum term (next a) next b))))
 
 
 (define (integral-simpson f a b n)
   (define h (/ (- b a) n))
-  (define (next a k)
-    (display k)
-    (newline)
-    (+ a (* k h)))  
-  (sum f (next a 0) next b 0))
+  (define (next a)
+    (+ a (* 2 h)))  
+  (* (/ h 3) (+ (cube (next a))
+                (* 4 (sum f (+ a h) next b))
+                (* 2 (sum f (+ a h) next b))
+                (cube b))))
           
 
 
 ; Точное значение интеграла cube от 0 до 1 равно 1/4.
 
-
-(integral-simpson cube 0 1.0 100)
-
-
+(integral-simpson cube 0 1.0 100) ; при n = 100
+(integral-simpson cube 0 1.0 1000) ; при n = 1000
