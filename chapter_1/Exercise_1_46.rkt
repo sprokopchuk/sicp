5af96f15d145b486e6e95d0abf4b5a8b258bf6cc
Sergey Prokopchuk <svp@anahoret.com>
2016-01-26 17:45:12 +0200

    Exercise_1_46.Solve with some issues.

---
+++ chapter_1/Exercise_1_46.rkt
@@ -0,0 +1,63 @@
+#lang racket
+
+(require rackunit)
+
+(define (average a b)(/ (+ a b) 2))
+(define (square x)(* x x))
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (repeated f n)
+  (if (> n 1)
+      (compose (repeated f (- n 1)) f)
+      (lambda (x) (f x))))
+
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+; Напишите процедуру iterative- improve, которая принимает в качестве аргументов две процедуры:
+; проверку, достаточно ли хоро- шо значение, и метод улучшения значения. Iterative-improve должна
+; возвращать процедуру, которая принимает начальное значение в качестве аргумента и улучшает его,
+; пока оно не станет достаточно хорошим. 
+
+(define (iterative-improve close-enough? f)
+  (lambda (x)
+    (let ((next (f x)))
+      (if (close-enough? x next)
+          next
+          ((iterative-improve close-enough? f) next)))))
+
+
+  (define (close-enough? v1 v2)
+    (define tolerance 0.00001)
+    (< (abs (- v1 v2)) tolerance))
+
+; Процедура написана с помощью iterative-improve
+(define (fixed-point f first-guess)
+  ((iterative-improve close-enough? f) first-guess))
+
+; Процедура для нахождения корней n-й степени
+; (n/2) раза применения процедуры average-damp вполне достаточно для торможения усреднением, чтобы
+; вычислить корень n-й степени
+
+(define (root x n)
+  (fixed-point ((repeated average-damp (/ n 2)) (lambda (y) (/ x (expt y (- n 1)))))
+               1.0))
+
+
+(root 590049 10) ; 3^10 = 590049
+
+(define (improve x)
+  (average-damp (lambda (y) (/ x y))))
+
+
+(define (good-enough? guess x)
+  (< (abs (- guess (improve x))) 1e-10))
+
+; Процедура переписана с помощью iterative-improve
+(define (sqrt x)
+  ((iterative-improve good-enough? improve) 1.0))
+
+
+(sqrt 9.0)
